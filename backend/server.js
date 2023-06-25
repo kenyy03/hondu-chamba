c5d49b09a627415a414d03423bd4b684f801d0bf
Keny Travanino <keny03@unitec.edu>
2023-06-24 19:00:19 -0600

    Agregando evento para web sockets

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const config = require('./config/index').config;
 const dbConnection = require('./config/database.config');
 const http = require('http');
+const sockets = require('./sockets');
 const { Server:WebSocketServer } = require('socket.io')
 
 dbConnection.connect();
@@ -13,4 +14,10 @@ const port = config.PORT || 5000;
 
 const server  = http.createServer(app);
 const httpServer = server.listen(port, () => console.log(`Server running on port ${port}`));
-const io = new WebSocketServer(httpServer);
+const io = new WebSocketServer(httpServer, {
+  cors: {
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000']
+
+  } 
+});
+sockets(io);
