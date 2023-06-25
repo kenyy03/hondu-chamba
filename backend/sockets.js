232f068cb136d2f1a5eec23ae81a2f3ed4b7deda
Keny Travanino <keny03@unitec.edu>
2023-06-25 00:45:10 -0600

    Agregando nombre usuario a respues de web socket

--- backend/sockets.js
+++ backend/sockets.js
@@ -1,6 +1,13 @@
 module.exports = (io) => {
   io.on('connection', (socket) => {
-    console.log(socket.id);
     console.log('a user connected');
+
+    socket.on('message', (body) => {
+      console.log('message received', body.body);
+      socket.broadcast.emit('message', {
+        body: body.body,
+        from: body.from,
+      });
+    } );
   })
 };
